989a10c138b2a8126bd3d13f589802c6f2a6f7d8
Jasmine Chen <jasmine1226@gmail.com>
2019-05-13 02:24:31 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -60,7 +60,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday|
     puts "#{season.capitalize}:"
      holiday_hash[season].each do |holiday, supply|
-       puts "  #{holiday.capitalize}: #{supply}"
+       puts "  #{holiday.capitalize}: #{supply.to_s}"
      end
   end
 
