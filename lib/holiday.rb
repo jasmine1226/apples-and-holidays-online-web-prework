2a73e9f10072708a60ed3ae964f458f196805925
Jasmine Chen <jasmine1226@gmail.com>
2019-05-13 02:58:47 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -81,8 +81,8 @@ def all_holidays_with_bbq(holiday_hash)
   bbq_holidays = []
   holiday_hash.each do |season, holiday|
     holiday_hash[season].each do |holiday, supply|
-      if holiday_hash[season][holiday].values.include?("BBQ")
-        bbq_holidays << holiday_hash[season][holiday].
+      if holiday_hash[season][holiday].values.include?("BBQ") == true
+        bbq_holidays << holiday
       end
     end
 end
